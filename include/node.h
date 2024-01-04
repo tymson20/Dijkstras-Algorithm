73f575ecbcbdfe860dd3f65cb4d656d800bd5d93
Tymek <tdomagala@student.agh.edu.pl>
2024-01-04 17:59:27 +0100

    Added new class Node

---
+++ include/node.h
@@ -0,0 +1,17 @@
+#ifndef NODE_H
+#define NODE_H
+
+class Node {
+public:
+    static int s_Id;
+
+public:
+    Node() : m_Id(s_Id) { s_Id++; }
+
+    int getId() const { return m_Id; }
+
+private:
+    int m_Id;
+};
+
+#endif // NODE_H
